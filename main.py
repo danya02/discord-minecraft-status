e5208d2cb9f9635241c3ab98609819d7c30b712c
Danya Generalov <danya@danya02.ru>
2021-04-04 21:30:24 +0300

    Adding logging to stderr.

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
 import discord
 from discord.ext import commands
 from discord_slash import SlashCommand, SlashContext
+import os
 
-bot = commands.Bot(command_prefix="!", intents=discord.Intents.all())
+import logging
+
+logging.basicConfig(level=logging.DEBUG)
+
+DISCORD_TOKEN = os.getenv('DISCORD_TOKEN') or None
+
+bot = commands.Bot(command_prefix="!")
 slash = SlashCommand(bot)
 
-@slash.slash(name="test")
+@slash.slash(name="test", guild_ids=[775744109359923221])
 async def _test(ctx: SlashContext):
     embed = discord.Embed(title="embed test")
     await ctx.send(content="test", embeds=[embed])
 
-bot.run("discord_token")
+bot.run(DISCORD_TOKEN)
